9a872db903baf4856ca6a764e77d81523a4fb094
이상준 <lsj032@securitynet.co.kr>
2022-09-05 18:25:45 +0900

    # test

--- js/main.js
+++ js/main.js
@@ -121,25 +121,6 @@
         loop: true,
     });
 
-    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(37.556282163394286, 126.8368786459305), //지도의 중심좌표.
-        level: 5 //지도의 레벨(확대, 축소 정도)
-    };
-
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-    // 마커가 표시될 위치입니다
-    var markerPosition  = new kakao.maps.LatLng(37.556282163394286, 126.8368786459305);
-
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
-
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-
     $('.data-copy').on('click', function() {
         var acctNum = $(this).data('acct-num')
 
@@ -152,6 +133,25 @@
             alert('복사가 완료됐습니다.')
         }
     });
+
+    // var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    // var options = { //지도를 생성할 때 필요한 기본 옵션
+    //     center: new kakao.maps.LatLng(37.556282163394286, 126.8368786459305), //지도의 중심좌표.
+    //     level: 5 //지도의 레벨(확대, 축소 정도)
+    // };
+
+    // var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    // // 마커가 표시될 위치입니다
+    // var markerPosition  = new kakao.maps.LatLng(37.556282163394286, 126.8368786459305);
+
+    // // 마커를 생성합니다
+    // var marker = new kakao.maps.Marker({
+    //     position: markerPosition
+    // });
+
+    // // 마커가 지도 위에 표시되도록 설정합니다
+    // marker.setMap(map);
 
 })(jQuery);
 
